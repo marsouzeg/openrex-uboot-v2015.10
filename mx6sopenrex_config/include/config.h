bee0615278da09634f35c80f2f4fc6893b3478d3
lyesouzeg <lyes_ouzeg10@yahoo.fr>
2018-03-18 14:40:28 +0100

    Initial version

---
+++ mx6sopenrex_config/include/config.h
@@ -0,0 +1,12 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/fedevel/mx6openrex/mx6s_2x_mt41k128.cfg
+#define CONFIG_MX6S	1
+#define CONFIG_DDR_SIZE	SZ_512M
+#define CONFIG_BOARD_TYPE_PREFIX	"imx6s-"
+#define CONFIG_BOARD_TYPE_POSTFIX	""
+#define CONFIG_BOARDDIR board/fedevel/mx6openrex
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/mx6openrex.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ mx6sopenrex_config/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,3 @@
+#define U_BOOT_DATE "Mar 18 2018"
+#define U_BOOT_TIME "06:25:19"
+#define U_BOOT_TZ "+0100"
